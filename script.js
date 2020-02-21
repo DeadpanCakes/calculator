65c644b16181f6561e7844ca68a5d35c98e4d213
Anthony Mendoza <anthony.mendoza310@gmail.com>
2020-02-21 13:53:34 -0800

    added event listener to buttons, giving them a function to return their text content

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/*
+functions for add, subtract, multiply, and divide
+function for operate, to consolidate operators
+function to store numbers in a variable
+funciton to concatenate stored number with subsequent input numbers
+function to break string of numbers when operator is input
+function to display strings of numbers and operators
+function that resolves operation when second operator is input or when equal sign is input
+function to clear display and reinitialize variables when C is pressed
+function to slice last num when backspace is pressed
+*/
 function add (x,y) {
     return x + y;
 }
@@ -24,4 +35,10 @@ function operate (operator, x, y) {
     } else if (operator == "divide") {
         return divide(x,y);
     }
+}
+
+let numbers = document.getElementsByClassName("numbers")
+for (i=0;i<numbers.length;i++) {
+    let text = numbers[i].textContent
+    numbers[i].addEventListener("click", () => console.log(text))
 }
